6aa2a8d15f450e6cc074109d7618922b5ffd9209
JiyeonChoi97 <57701500+JiyeonChoi97@users.noreply.github.com>
2020-04-29 18:26:30 +0900

    20-Session

---
+++ 20-Session/exam1.py
@@ -0,0 +1,18 @@
+import requests
+
+url = 'https://datalab.naver.com/keyword/realtimeList.naver?where=main'    # 접근 불가
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
+session = requests.Session()
+session.headers.update({'User_agent':user_agent, 'referer':None})
+
+r = session.get(url)
+
+if r.status_code != 200:
+    print('[Error]')
+    quit()
+    
+print(r.text)
+print('-' * 30)
+
+with open('naver급상승 검색어.txt', 'w', encoding='utf-8') as f:
+    f.write(r.text)
